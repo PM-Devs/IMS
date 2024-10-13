13d7c76f2b6f447987cba3ac3390a1edf9653de0
PM-Devs <princegh100@outlook.com>
2024-10-13 15:06:52 +0000

    Allows population by alias.

--- services/service.py
+++ services/service.py
@@ -62,7 +62,9 @@ def verify_password(plain_password, hashed_password):
 
 async def get_user(email: str):
     user_dict = await db.users.find_one({"email": email})
-    print("User dict:" ,user_dict)
+        # Convert _id to id
+    if user_dict and '_id' in user_dict:
+        user_dict['id'] = str(user_dict.pop('_id')) 
     if user_dict:
         return User(**user_dict)
 
